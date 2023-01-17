8dcbef56396e16978f4132a3f2961f1c5f403aa8
Lee Jung Min <73169929+LEE-jm96@users.noreply.github.com>
2023-01-18 06:27:23 +0900

    체육복

--- 20230117.js
+++ 20230117.js
@@ -30,8 +30,6 @@ function solution(n, lost, reserve) {
     for(let i = 0; i < lost.length; i++){
         if(reserve.includes(lost[i] - 1)){
             reserve = reserve.filter((item) => item !== lost[i] - 1)
-        }else if(reserve.includes(lost[i])){
-            reserve = reserve.filter((item) => item !== lost[i])
         }else if(reserve.includes(lost[i] + 1)){
             reserve = reserve.filter((item) => item !== lost[i] + 1)
         }else{
